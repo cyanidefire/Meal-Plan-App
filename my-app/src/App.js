22b2f1c2029474280c622a7c7154beae0726a7ce
cyanidefire <lucy16813d@gmail.com>
2026-09-12 19:56:14 +0100

    mui and other things

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          will this work instead
+          meow
         </p>
         <a
           className="App-link"
